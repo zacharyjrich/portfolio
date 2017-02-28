b59e063a1a01dc5e58212ce1133e762f9f6d2959
Zachary Richards <zachary.j.richards@gmail.com>
2017-02-28 00:15:08 -0800

    adjust links on hero body area

--- js/app.js
+++ js/app.js
@@ -1,50 +1,50 @@
-
 $(document).foundation();
 
 (function($) {
-  $.fn.visible = function(partial) {
-    var $t            = $(this),
-        $w            = $(window),
-        viewTop       = $w.scrollTop(),
-        viewBottom    = viewTop + $w.height(),
-        _top          = $t.offset().top;
+    $.fn.visible = function(partial) {
+        var $t = $(this),
+            $w = $(window),
+            viewTop = $w.scrollTop(),
+            viewBottom = viewTop + $w.height(),
+            _top = $t.offset().top;
 
-    return (viewBottom >= _top);
-  };
+        return (viewBottom >= _top);
+    };
 })($);
 
 var $primeItems = [
-  '.hero-body .nav-link',
-  '.projects .title',
+    '.header-bar',
+    '.hero-body',
+    '.projects .title',
 ];
 
 for (var i = 0, l = $primeItems.length; i < l; i++) {
-  var $primeItem = $($primeItems[ i ]);
-  if( !$primeItem.hasClass('nofade') ) {
-		$primeItem.addClass('content-primed');
-  }
+    var $primeItem = $($primeItems[i]);
+    if (!$primeItem.hasClass('nofade')) {
+        $primeItem.addClass('content-primed');
+    }
 }
 
 var win = $(window);
 var $allMods = $('.content-primed');
 
 $(document).ready(function() {
-  // Already visible content
-  $allMods.each(function(i, el) {
-    $allMods = $('.content-primed');
-    var $el = $(el);
-    if ($el.visible(true)) {
-      $el.addClass('fade-in').removeClass('content-primed');
-    }
-  });
-
-  win.scroll(function(event) {
-    $allMods = $('.content-primed');
+    // Already visible content
     $allMods.each(function(i, el) {
-      var $el = $(el);
-      if ($el.visible(true)) {
-        $el.addClass('fade-in').removeClass('content-primed');
-      }
+        $allMods = $('.content-primed');
+        var $el = $(el);
+        if ($el.visible(true)) {
+            $el.addClass('fade-in').removeClass('content-primed');
+        }
     });
-  });
+
+    win.scroll(function(event) {
+        $allMods = $('.content-primed');
+        $allMods.each(function(i, el) {
+            var $el = $(el);
+            if ($el.visible(true)) {
+                $el.addClass('fade-in').removeClass('content-primed');
+            }
+        });
+    });
 });
